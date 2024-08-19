8a04a1939aa10012415d5c7d1252555eb0b08abd
Zimbru Florin <florinz@cicadatech.eu>
2024-08-20 00:25:31 +0300

    fx

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -34,7 +34,7 @@ class _LoginPageState extends State<LoginPage> {
     return Scaffold(
       appBar: AppBar(
         automaticallyImplyLeading: false,
-        title: Text("Login"),
+        title: Text(""),
       ),
       body: Center(
         child: Padding(
