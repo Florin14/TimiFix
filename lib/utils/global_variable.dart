3547d4db3675da3bdb344a25a817957bd85083a8
florinz <florinz@cicadatech.eu>
2025-01-26 17:30:39 +0200

    FZ/commit

--- lib/utils/global_variable.dart
+++ lib/utils/global_variable.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:timi_fix/screens/home_page.dart';
 
 const webScreenSize = 600;
 
@@ -7,6 +8,7 @@ List<Widget> homeScreenItems = [
   // const FeedScreen(),
   // const SearchScreen(),
   // const AddPostScreen(),
+  const HomePage(),
   const Text('notifications'),
   // ProfileScreen(
   //   uid: FirebaseAuth.instance.currentUser!.uid,
